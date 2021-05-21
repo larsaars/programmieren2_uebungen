efa6993da4474bdabe74c5af71eef2d3f4f6be03
Lurzapps <lurzapps@gmail.com>
2021-05-21 23:15:11 +0200

    dynamic_array

--- ue9/task3/dynamic_array.cpp
+++ ue9/task3/dynamic_array.cpp
@@ -1,20 +1,68 @@
+#include <iostream>
 #include "dynamic_array.h"
 
-using std::move;
+using std::move, std::endl, std::cerr, std::cout;
 
-template<typename T> const dynamic_array<T> &dynamic_array<T>::operator+=(const dynamic_array &rhs) {
-    auto *temp = new T[len + rhs.len];
+// operator to concatenate two dynamic_arrays:
+// this array will be edited, the rhs will be copied
+template<typename T>
+const dynamic_array<T> &dynamic_array<T>::operator+=(const dynamic_array &rhs) {
+    // fix length attribute in this scope
+    len += rhs.len;
+    // create new temporary array with new length
+    T *temp = new T[len + rhs.len];
 
-    for(int i = 0; i < len; i++)
+    // move lhs to newly allocated arrays
+    for (int i = 0; i < len; i++)
         temp[i] = move(arr[i]);
 
-    for(int i = 0; i < rhs.len; i++)
+    // copy rhs to new temp array
+    for (int i = 0; i < rhs.len; i++)
         temp[i + len] = rhs.arr[i];
 
-    len += rhs.len;
-
-    delete [] arr;
+    // delete empty pointer
+    delete[] arr;
+    // reset temp
     arr = temp;
+
+    // return this scope
+    return *this;
+}
+
+// print out list
+template<typename T>
+std::ostream &operator<<(std::ostream &out, const dynamic_array<T> &a) {
+    out << "[";
+    for (int i = 0; i < a.len; i++) {
+        if (i != 0)
+            out << ", ";
+        out << a.arr[i];
+    }
+    out << "]";
+    return out;
+}
+
+// access T via index
+template<typename T>
+T &dynamic_array<T>::operator[](size_t index) {
+    if (index < 0 || index >= len)
+        cerr << "IndexOutOfBoundsException: " << index << " / " << len << endl;
+    return arr[index];
+}
+
+// get index via T
+template<typename T>
+int dynamic_array<T>::operator[](const T &t) {
+    for (int i = 0; i < len; ++i)
+        if (t == arr[i])
+            return i;
+    return -1;
+}
+
+// aka the append function of the list
+// add items of type T to list
+template<typename T>
+const dynamic_array<T> &dynamic_array<T>::operator+=(const T &rhs) {
 
     return *this;
 }
--- ue9/task3/dynamic_array.h
+++ ue9/task3/dynamic_array.h
@@ -2,11 +2,12 @@
 #define TASK3_DYNAMIC_ARRAY_H
 
 
+#include <ostream>
 #include <string>
 
-using std::string;
 
-template<typename T> class dynamic_array {
+template<typename T>
+class dynamic_array {
 private:
     T *arr;
     int len;
@@ -15,14 +16,31 @@ public:
     dynamic_array() : len(0), arr(nullptr) {}
 
     dynamic_array(int size) : len(size) {
-        arr = new string[size];
+        arr = new T[size];
+    }
+
+    dynamic_array(const T &t) : dynamic_array(1) {
+        arr[0] = t;
+    }
+
+    dynamic_array(T arr0[]){
+        arr = arr0;
+        len = sizeof(arr) / sizeof(arr[0]);
     }
 
     ~dynamic_array() {
         delete[] arr;
     }
 
+    T &operator[](size_t index);
+
+    int operator[](const T &t);
+
     const dynamic_array &operator+=(const dynamic_array &rhs);
+
+    const dynamic_array &operator+=(const T &rhs);
+
+    friend std::ostream &operator<<(std::ostream &out, const dynamic_array<T> &a);
 };
 
 #endif //TASK3_DYNAMIC_ARRAY_H
