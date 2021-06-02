606fb139ab31e34d4b6717ec89654b46bafe53fa
larsaars <lurzapps@gmail.com>
2021-06-02 17:25:04 +0200

    ue11 in uebung task 1, 2

--- ue11/task2/main.cpp
+++ ue11/task2/main.cpp
@@ -6,68 +6,93 @@
 
 using std::cout, std::endl, std::vector, std::string, std::cerr;
 
+// (A)
 template<typename T>
-void reverse(vector<T> &vector1) {
-    int counter = vector1.size();
-    int s = vector1.size();
-    if (vector1.size() % 2 == 1)
+void reverse(vector<T> &v) {
+    int counter = v.size(), size = counter;
+
+    if (size % 2 == 1)
         counter--;
 
     for (int i = 0; i < (counter / 2); i++) {
-        T temp = vector1[i];
-        vector1[i] = vector1[s - 1 - i];
-        vector1[s - 1 - i] = temp;
+        T temp = v[i];
+        v[i] = v[size - 1 - i];
+        v[size - 1 - i] = temp;
     }
 }
 
-int vector_max(vector<int> &vector1) {
-    if (vector1.size() == 0) {
+// (B)
+int vector_max(const vector<int> &v) {
+    if (v.empty()) {
         cerr << "No Element in vector!";
         return -1;
     }
 
-    int max_int = vector1[0];
-    for (int i = 1; i < vector1.size(); i++) {
-        max_int = vector1[i] > max_int ? vector1[i] : max_int;
-    }
+    int max_int = v[0];
+    for (int i = 1; i < v.size(); i++)
+        if (v[i] > max_int)
+            max_int = v[i];
+
+    // max_int = v[i] > max_int ? v[i] : max_int;
+
     return max_int;
 }
 
-template<typename T>
-T max_value(vector<T> &vector1, T (*func)(vector <T>)
 
-) {
-return
-func(vector1);
+// (C)
+template<typename T>
+int largest(const vector<T> &v, int (*sizeFunc)(const T &)) {
+    if (v.empty()) {
+        cerr << "No Element in vector!";
+        return -1;
+    }
+
+    int maxValue = sizeFunc(v[0]), maxIndex = 0;
+    for (int i = 1; i < v.size(); i++) {
+        int sizeI = sizeFunc(v[i]);
+
+        if (sizeI > maxValue) {
+            maxValue = sizeI;
+            maxIndex = i;
+        }
+    }
+
+    return maxIndex;
 }
 
+int stringlength(const string &s) { return s.length(); }
+
+
 int main() {
+    // (A)
     vector<int> v1;
-    for (int i = 1; i <= 10; i++) {
+    for (int i = 1; i <= 10; i++)
         v1.push_back(i);
-    }
 
     reverse(v1);
 
-    for (int i = 0; i < v1.size(); i++)
-        cout << v1[i] << " ";
+    for (int i : v1)
+        cout << i << " ";
     cout << endl << endl;
 
     vector<int> v2;
-    for (int i = 1; i <= 9; i++) {
+    for (int i = 1; i <= 9; i++)
         v2.push_back(i);
-    }
 
     reverse(v2);
 
-    for (int i = 0; i < v2.size(); i++)
-        cout << v2[i] << " ";
+    for (int i : v2)
+        cout << i << " ";
     cout << endl << endl;
 
-    //vector<int> v3;
+    // (B)
     cout << "Max value of int vector: " << vector_max(v2) << endl << endl;
 
-    cout << "Max value of int vector by using a template: " << max_value(v2, vector_max) << endl;
+
+    // (C)
+    vector<string> strings = { "Schreiben", "Sie", "eine", "Template -Funktion", "die" };
+    int index = largest(strings, stringlength); // == 3 ?
+    cout << index << endl;
 
     return 0;
 }
