680b5295b7cfcd02da574c489764d193d95aa13c
larsaars <lurzapps@gmail.com>
2021-07-01 23:48:41 +0200

    klausur aufgabe 7

--- klausur/7/main.cpp
+++ klausur/7/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
+#include <exception>
 #include <string>
 
-using std::cout, std::cerr, std::endl, std::string, std::move;
+using std::cout, std::cerr, std::endl, std::string, std::move, std::exception;
 
 template <typename T> struct d_arr {
   public:
@@ -16,11 +17,24 @@ template <typename T> struct d_arr {
     void append(const T &element);
 };
 
+class price_mismatch : public exception {
+
+};
+
+class missing_price : public exception {
+
+};
+
 class item {
   public:
     string producer, name;
-    int count;
-    float value;
+    int count = 1;
+    float price;
+
+    item() {}
+
+    item(const string &producer, const string &name, float price = -1) : 
+        producer(producer), name(name), price(price) {}
 };
 
 class inventory {
@@ -35,11 +49,11 @@ class inventory {
 
     // not neccessary since d_arr is allocated as an automtic variable
     // which will be deleted when the object goes out of context
-    ~inventory() {}
+    ~inventory() = default; 
 
     void add_item(item *i) { items.append(i); }
 
-    void add_item(const string &producer, const string &name, float price = 0);
+    void add_item(const string &producer, const string &name, float price = -1);
 };
 
 inventory::inventory(inventory &&temp) {
@@ -49,6 +63,25 @@ inventory::inventory(inventory &&temp) {
 }
 
 void inventory::add_item(const string &producer, const string &name,
-                         float price) {}
+                         float price) {
+    for(int i = 0; i < items.size; i++) {
+       item *current = items.data[i];
+
+       if(current->producer == producer &&
+               current->name == name) {
+           
+           if(price != -1 && current->price != price)
+               throw price_mismatch();
+
+           current->count++;
+           return;
+       }
+    }
+    
+    if(price == -1)
+        throw missing_price();
+
+    items.append(new item(producer, name, price));
+}
 
 int main(int argc, char **argv) { cout << "test" << endl; }
