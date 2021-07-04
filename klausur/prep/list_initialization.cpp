2d4222790500baac220b0491c06762b4a2be397d
larsaars <lurzapps@gmail.com>
2021-07-04 18:09:50 +0200

    initializer lists

--- klausur/prep/list_initialization.cpp
+++ klausur/prep/list_initialization.cpp
@@ -10,6 +10,8 @@ struct A  {
 //     A() {}
 //     A(int i1, int i2) : i1(i1), i2(i2) {}
 
+//     A(initializer_list<int> &&init) {}
+
     void print() {
         cout << i1 << ", " << i2 << ", " << i3 << ", " << s << endl;
     }
