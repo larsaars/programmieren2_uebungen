9e7a9372611c41be1c4d9a4175e23a3bae7813d2
larsaars <lurzapps@gmail.com>
2021-07-04 16:50:11 +0200

    test list init

--- klausur/prep/list_initialization.cpp
+++ klausur/prep/list_initialization.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 struct A  {
     int i1, i2, i3;
+    string s;
 
     void print() {
-        cout << i1 << ", " << i2 << ", " << i3 << endl;
+        cout << i1 << ", " << i2 << ", " << i3 << ", " << s << endl;
     }
 };
 
 int main() {
-    A a{1, 2, 3};
+    A a{1, 2, 3, "test"};
     a.print();
 }
